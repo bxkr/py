214e4e81c781bb34b0845b44c0aafc453861781e
Andrew <bxkr@mail.com>
2021-08-02 20:15:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def print_a1z26(*combination):
+    alphabet = list(map(chr, range(ord('a'), ord('z')+1)))
+    for number in combination:
+        if number > 0:
+            print(alphabet[number-1], end='')
+        elif number == 0:
+            print(' ', end='')
+    print('\n', end='')
+
+
+if __name__ == '__main__':
+    print_a1z26(8, 5, 12, 12, 15, 0, 23, 15, 18, 12, 4)
